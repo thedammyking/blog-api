241d74cdfbfb65431a5fa337df72931fd3d59151
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-06-25 13:28:45 +0100

    added Saved Post Repository

---
+++ src/api/v1/Repositories/SavedPostRepository.ts
@@ -0,0 +1,56 @@
+import { and, eq, isNotNull, isNull, sql } from 'drizzle-orm';
+
+import { db } from '@/db';
+import * as schema from '@/db/schema';
+
+class SavedPostRepository {
+  private db = db;
+
+  async add(readerId: string, postId: string) {
+    return await this.db.insert(schema.savedPost).values({ readerId, postId }).returning();
+  }
+
+  async update(readerId: string, postId: string) {
+    return await this.db
+      .update(schema.savedPost)
+      .set({ deletedAt: null })
+      .where(
+        and(
+          eq(schema.savedPost.readerId, readerId),
+          eq(schema.savedPost.postId, postId),
+          isNotNull(schema.savedPost.deletedAt)
+        )
+      )
+      .returning();
+  }
+
+  async getAllByReaderId(readerId: string) {
+    return await this.db
+      .select()
+      .from(schema.savedPost)
+      .where(and(eq(schema.savedPost.readerId, readerId), isNull(schema.savedPost.deletedAt)));
+  }
+
+  async getAllByPostId(postId: string) {
+    return await this.db
+      .select()
+      .from(schema.savedPost)
+      .where(and(eq(schema.savedPost.postId, postId), isNull(schema.savedPost.deletedAt)));
+  }
+
+  async delete(readerId: string, postId: string) {
+    return await this.db
+      .update(schema.savedPost)
+      .set({ deletedAt: sql`now()` })
+      .where(
+        and(
+          eq(schema.savedPost.readerId, readerId),
+          eq(schema.savedPost.postId, postId),
+          isNull(schema.savedPost.deletedAt)
+        )
+      )
+      .returning();
+  }
+}
+
+export default SavedPostRepository;
