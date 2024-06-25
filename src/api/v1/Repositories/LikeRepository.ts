ca2ad5272e89b803aa22339af698cef4718f02e9
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-06-25 13:28:31 +0100

    added Like Repository

---
+++ src/api/v1/Repositories/LikeRepository.ts
@@ -0,0 +1,56 @@
+import { and, eq, isNotNull, isNull, sql } from 'drizzle-orm';
+
+import { db } from '@/db';
+import * as schema from '@/db/schema';
+
+class LikeRepository {
+  private db = db;
+
+  async add(readerId: string, postId: string) {
+    return await this.db.insert(schema.like).values({ readerId, postId }).returning();
+  }
+
+  async update(readerId: string, postId: string) {
+    return await this.db
+      .update(schema.like)
+      .set({ deletedAt: null })
+      .where(
+        and(
+          eq(schema.like.readerId, readerId),
+          eq(schema.like.postId, postId),
+          isNotNull(schema.like.deletedAt)
+        )
+      )
+      .returning();
+  }
+
+  async getAllByReaderId(readerId: string) {
+    return await this.db
+      .select()
+      .from(schema.like)
+      .where(and(eq(schema.like.readerId, readerId), isNull(schema.like.deletedAt)));
+  }
+
+  async getAllByPostId(postId: string) {
+    return await this.db
+      .select()
+      .from(schema.like)
+      .where(and(eq(schema.like.postId, postId), isNull(schema.like.deletedAt)));
+  }
+
+  async delete(readerId: string, postId: string) {
+    return await this.db
+      .update(schema.like)
+      .set({ deletedAt: sql`now()` })
+      .where(
+        and(
+          eq(schema.like.readerId, readerId),
+          eq(schema.like.postId, postId),
+          isNull(schema.like.deletedAt)
+        )
+      )
+      .returning();
+  }
+}
+
+export default LikeRepository;
